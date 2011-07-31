1932dc3b93c54c80d6f27446483f377fa41da5bf
Samuel Rivas <samuelrivas@gmail.com>
2011-07-31 18:09:23 +0200

    FUNCT first commit

---
+++ js/delicatessen.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    var testString = document.createElement("p");
+    testString.textContent = "Delicatessen is work in progress!";
+    document.body.appendChild(testString)
+}
