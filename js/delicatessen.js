f9c1717f1410d0f6e51268a650ebcf7600cc9efe
Samuel Rivas <samuelrivas@gmail.com>
2011-08-07 18:01:36 +0200

    FUNCT a very basic experiment to test that we can use delicious api
    It just prompts (ugly way) your username and password and queries the api to
    download all your tags (can be slow)

--- js/delicatessen.js
+++ js/delicatessen.js
@@ -1,5 +1,23 @@
 window.onload = function() {
     var testString = document.createElement("p");
+    var user;
+    var passwd;
+
     testString.textContent = "Delicatessen is work in progress!";
-    document.body.appendChild(testString)
+    document.body.appendChild(testString);
+
+    user = window.prompt("Write your user name", "User Name");
+    passwd = window.prompt("Write your password", "Password");
+
+    http = new XMLHttpRequest();
+    http.open("GET", "https://api.del.icio.us/v1/tags/get", true, user, passwd);
+    http.onreadystatechange =
+	function() {
+	    var xmlText = document.createElement("p");
+	    console.log("Received http response");
+	    xmlText.textContent = http.response;
+	    document.body.appendChild(xmlText);
+	};
+    console.log("sending http request");
+    http.send();
 }
