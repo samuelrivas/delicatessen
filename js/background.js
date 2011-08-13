f55e06a154ab5f04df91c1c203436709a157cf3b
Samuel Rivas <samuelrivas@gmail.com>
2011-08-13 18:41:16 +0200

    FUNCT added DELICT_BG.get_tags that gets user and password from the state
    Most of the functionality is copied from delicatessen.js (we will remove it from
    there later)
    
    The main reason to move this to the background are:
    
     * We cache the tags to avoid querying the api (and waiting a few seconds) each
       time we open the popup
     * We don't want to expose get_passwd through the requests api. For that so all
       privileged functions will run in the background closure
    
    Something that was wrong in the previous implementation is that tags was an
    array of nodes instead of an array of strings. The array of nodes couldn't be
    stringified by JSON (it had circular references), and thus couldn't be sent as
    request result. Also, it seemd to be way heavier to handle.

--- js/background.js
+++ js/background.js
@@ -3,6 +3,7 @@ var DELICT_BG = function() {
     // -----------------
     var user = null;
     var passwd = null;
+    var tags = null;
 
     var that = {};
 
@@ -20,6 +21,19 @@ var DELICT_BG = function() {
     };
     that.set_user = function(new_user) { user = new_user };
     that.set_passwd = function(new_passwd) { passwd = new_passwd };
+
+    that.get_tags = function(callback) {
+        if (tags) {
+            callback(tags);
+        } else {
+            DELICT_BG.get_tags_internal(
+                that.get_user(), get_passwd(),
+                function(t) {
+                    tags = t;
+                    callback (t);
+                });
+        }
+    };
 
     // Group functions exported to the outside through
     // chrome.extension.sendRequest in the request_handler inner object to ease
@@ -49,6 +63,53 @@ DELICT_BG.handle_request = function(request, sender, send_response) {
         send_response("unsupported");
     }
 }
+
+DELICT_BG.get_document = function(url, user, passwd, on_received) {
+    var rpc_request;
+
+    rpc_request = new XMLHttpRequest();
+    rpc_request.open("GET", url, true, user, passwd);
+
+    rpc_request.onreadystatechange = function() {
+        console.log("New request state: " + rpc_request.readyState);
+        if (rpc_request.readyState === 4) {
+            if (rpc_request.status !== 200) {
+                throw "Wrong HTTP status: " + rpc_request.status + " "
+                    + rpc_request.statusText
+            }
+            console.log("Received HTTP response");
+            on_received(rpc_request.responseXML);
+        }
+    }
+
+    console.log("sending HTTP request");
+    rpc_request.send();
+};
+
+DELICT_BG.get_tags_internal = function(user, passwd, on_received) {
+    var callback = function(tags_doc) {
+        console.log("Parsing XML tags document");
+        on_received(DELICT_BG.parse_tags(tags_doc));
+    }
+    this.get_document(
+        "https://api.del.icio.us/v1/tags/get", user, passwd, callback);
+},
+
+// Internal stuff
+//===============
+
+// XXX This function could be heavy, but my tests with near to 2000 tags
+// show that is not worth asynchronising it
+DELICT_BG.parse_tags = function(tags_doc) {
+    var nodes = tags_doc.getElementsByTagName("tag");
+    var tags = [];
+    var i;
+
+    for (i = 0; i < nodes.length; i++) {
+        tags.push(nodes[i].attributes["tag"].nodeValue);
+    }
+    return tags;
+};
 
 // Add the request listener
 chrome.extension.onRequest.addListener(DELICT_BG.handle_request);
