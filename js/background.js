621540c5406018a301027af62c835ae0f2d89667
Samuel Rivas <samuelrivas@gmail.com>
2011-08-13 13:00:45 +0200

    FUNCT Implement password and user management as a background service
     * We use a simple closure to avoid exposing them as object properties
     * Export get_user, set_user and set_passwd as extension request
     * Don't expose get_passwd, only functions in the closure will be able to use it
     * Temporarily, get_passwd and get_user will fire a propmt if the values are not
       set (to ease testing)
     * All methods of the DELICT_BG.request_handler object are automatically
       exported through chrome.extension.sendRequest

--- js/background.js
+++ js/background.js
@@ -1,7 +1,54 @@
-chrome.extension.onRequest.addListener(
-    function (request, sender, send_response) {
-        console.log("Background received request:" + request);
-        send_response("unsupported")
-    }
-);
+var DELICT_BG = function() {
+    // Private variables
+    // -----------------
+    var user = null;
+    var passwd = null;
 
+    var that = {};
+
+    // Internal functions
+    var get_passwd = function() {
+        passwd = that.prompt_if_null(passwd, "Password");
+        return passwd;
+    };
+
+    // Privileged functions
+    // --------------------
+    that.get_user = function() {
+        user = that.prompt_if_null(user, "User Name");
+        return user;
+    };
+    that.set_user = function(new_user) { user = new_user };
+    that.set_passwd = function(new_passwd) { passwd = new_passwd };
+
+    // Group functions exported to the outside through
+    // chrome.extension.sendRequest in the request_handler inner object to ease
+    // our handle_request function
+    that.request_handler = {
+        set_user : that.set_user,
+        get_user : that.get_user,
+        set_passwd : that.set_passwd,
+    }
+    return that;
+}();
+
+// Unprivileged functions
+DELICT_BG.prompt_if_null = function(data, prompt_text) {
+    if (data === null) {
+        data = window.prompt(prompt_text);
+    }
+    return data;
+}
+
+DELICT_BG.handle_request = function(request, sender, send_response) {
+    if (DELICT_BG.request_handler[request.call]) {
+        console.log("Background received request:" + request.call);
+        send_response(DELICT_BG.request_handler[request.call](request.args));
+    } else {
+        console.log("Background received unsupported request: " + request);
+        send_response("unsupported");
+    }
+}
+
+// Add the request listener
+chrome.extension.onRequest.addListener(DELICT_BG.handle_request);
