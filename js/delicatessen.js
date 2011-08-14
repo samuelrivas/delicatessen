48578a3a09d3798faf58ff52b123405e29198ae2
Samuel Rivas <samuelrivas@gmail.com>
2011-08-14 15:59:45 +0200

    FUNCT render all tags when loading the popup

--- js/delicatessen.js
+++ js/delicatessen.js
@@ -1,12 +1,21 @@
 var DELICATESSEN = {
     get_tags : function(on_received) {
         chrome.extension.sendRequest({async_call : "get_tags"}, on_received);
+    },
+    render_tags : function(tags, parentNode) {
+        for (var i = 0; i < tags.length; i++) {
+            var div = document.createElement("div");
+            div.textContent = tags[i];
+            div.className = "tag";
+            parentNode.appendChild(div);
+        }
     }
 };
 
 window.onload = function() {
-    var test_p = document.createElement("p");
-
-    test_p.textContent = "Delicatessen is work in progress!";
-    document.body.appendChild(test_p);
+    DELICATESSEN.get_tags(
+        function(t) {
+            console.log("Got " + t.length + " tags");
+            DELICATESSEN.render_tags(t, document.body);
+        });
 };
