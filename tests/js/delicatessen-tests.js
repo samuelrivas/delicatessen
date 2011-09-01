8d9694a621af4b93c942856e530def63568b27d5
Samuel Rivas <samuelrivas@gmail.com>
2011-09-01 20:03:58 +0200

    RESTRUCT more elegant test code layout
    We separate actual test cases and the test suite to avoid brutal nesting
    
    Also do everything in a closure to avoid adding variables to the context

--- tests/js/delicatessen-tests.js
+++ tests/js/delicatessen-tests.js
@@ -1,41 +1,52 @@
-describe(
-    "Delicatessen API tests",
-    function() {
-        describe(
-            "User name test",
-            function() {
-                it(
-                    "must allow to set the user name and get it back",
-                    function() {
+(function() {
+    //--------------------------------------------------------------------
+    // Test cases
+    //--------------------------------------------------------------------
+    var set_get_user_name = {
+        descr : "must allow to set the user name and get it back",
+        test : function() {
 
-                        var user = null;
+            var user = null;
 
-                        runs(
-                            function() {
-                                DELICATESSEN.set_user("foo");
-
-                                DELICATESSEN.get_user(function(o) { user = o });
-                            });
-
-                        waitsFor(
-                            function() { return user !== null },
-                            "get_user never returned", 1000);
-
-                        runs(
-                            function() {
-                                expect(user).toBe("foo");
-
-                                DELICATESSEN.set_user("delicatessen")
-
-                                user = null;
-                                DELICATESSEN.get_user(function(o) { user = o });
-                            });
-
-                        waitsFor(
-                            function() { return user !== null },
-                            "get_user never returned", 1000);
-
-                        runs(function() { expect(user).toBe("delicatessen") });
-                    });
+            runs(function() {
+                DELICATESSEN.set_user("foo");
+                DELICATESSEN.get_user(function(o) { user = o });
             });
-    });
+
+            waitsFor(
+                function() { return user !== null },
+                "get_user never returned", 1000);
+
+            runs(function() {
+                expect(user).toBe("foo");
+
+                DELICATESSEN.set_user("delicatessen")
+
+                user = null;
+                DELICATESSEN.get_user(function(o) { user = o });
+            });
+
+            waitsFor(
+                function() { return user !== null },
+                "get_user never returned", 1000);
+
+            runs(function() { expect(user).toBe("delicatessen") });
+        }
+    };
+
+    //--------------------------------------------------------------------
+    // Auxiliary functions
+    //--------------------------------------------------------------------
+    var test_it = function(test_case) {
+        return function() { it(test_case.descr, test_case.test) };
+    }
+
+    //--------------------------------------------------------------------
+    // Test suites
+    //--------------------------------------------------------------------
+    describe(
+        "Delicatessen API tests",
+        function() {
+            describe("User name test", test_it(set_get_user_name));
+        });
+})();
